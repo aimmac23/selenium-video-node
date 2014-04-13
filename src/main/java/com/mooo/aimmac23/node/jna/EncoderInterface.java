776086e9255f7cf89b49ea0cf87d7102d64695d6
Alasdair Macmillan <aimmac23@gmail.com>
2014-04-13 20:56:22 +0100

    Also commit the JNA interface to the C library interface code

---
+++ src/main/java/com/mooo/aimmac23/node/jna/EncoderInterface.java
@@ -0,0 +1,29 @@
+package com.mooo.aimmac23.node.jna;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+
+
+public interface EncoderInterface extends Library {
+	EncoderInterface INSTANCE = (EncoderInterface)Native.loadLibrary("interface.so", EncoderInterface.class);
+	
+	Pointer create_context();
+	
+	int init_encoder(Pointer context, int width, int height);
+	
+	int init_codec(Pointer context);
+	
+	int init_image(Pointer context);
+	
+	int convert_frame(Pointer context, int[] data);
+	
+	int encode_next_frame(Pointer context);
+	
+	int encode_finish(Pointer context);
+	
+	String codec_error_detail(Pointer context);
+
+}
+	
+
