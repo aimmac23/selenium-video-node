81a8a1e5b915bb9842aa2554f8cc74b449fa2745
Alasdair Macmillan <aimmac23@gmail.com>
2017-11-13 22:39:15 +0000

    Commit some Hub integration tests. Note that this will definitely break the Selenium 2.x build

---
+++ src/test/java/com/aimmac23/hub/HubProxyIntegrationTest.java
@@ -0,0 +1,85 @@
+package com.aimmac23.hub;
+
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
+
+import java.util.Arrays;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.model.HttpRequest;
+import org.mockserver.socket.PortFactory;
+import org.mockserver.verify.VerificationTimes;
+import org.openqa.grid.internal.utils.configuration.GridHubConfiguration;
+import org.openqa.grid.web.Hub;
+import org.openqa.selenium.remote.internal.HttpClientFactory;
+import org.testng.Assert;
+
+import com.aimmac23.hub.servlet.HubVideoDownloadServlet;
+import com.aimmac23.hub.servlet.HubVideoInfoServlet;
+
+public class HubProxyIntegrationTest {
+
+	private static final HttpRequest RESET_REQUEST = HttpRequest.request("/extra/VideoRecordingControlServlet")
+			.withMethod("POST").withQueryStringParameter("command", "reset");
+	
+	private ClientAndServer mockServer;
+	private Hub hub;
+	private int hubPort;
+	private HttpClient httpClient;
+
+	private int mockClientPort;
+	
+	@Before
+	public void setup() throws Exception {
+		hubPort = PortFactory.findFreePort();
+		mockClientPort = PortFactory.findFreePort();
+
+	    mockServer = startClientAndServer(mockClientPort);
+	    GridHubConfiguration hubConfig = new GridHubConfiguration();
+	    
+	    hubConfig.servlets = Arrays.asList(HubVideoDownloadServlet.class.getCanonicalName(),
+	    		HubVideoInfoServlet.class.getCanonicalName());
+	    hubConfig.port = hubPort;
+	    
+	    
+	    hub = new Hub(hubConfig);
+	    hub.start();
+	    
+	    httpClient = new HttpClientFactory().getHttpClient();
+
+	}
+	
+	private void expectNodeServletCalls() {
+
+		mockServer.when(RESET_REQUEST)
+			.respond(org.mockserver.model.HttpResponse.response("OK").withStatusCode(200));
+		
+	}
+	
+	@Test
+	public void testBasicRegistration() throws Exception {
+		expectNodeServletCalls();
+		
+		String registrationRequest = String.format("{name: 'test', configuration: {proxy: 'com.aimmac23.hub.proxy.VideoProxy', remoteHost: 'http://127.0.0.1:%s'}}", mockClientPort);
+		HttpPost post = new HttpPost("http://127.0.0.1:" + hubPort + "/grid/register/");
+		post.setEntity(new StringEntity(registrationRequest));
+		
+		HttpResponse response = httpClient.execute(post);
+		try {
+			Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
+			EntityUtils.consumeQuietly(response.getEntity());
+		} finally {
+			post.releaseConnection();
+		}
+		Thread.sleep(2000);
+		
+		// check that the one HTTP call the VideoProxy makes on startup gets made
+		mockServer.verify(RESET_REQUEST, VerificationTimes.once());
+	}
+}
---
+++ src/test/java/com/aimmac23/hub/HubServletIntegrationTest.java
@@ -0,0 +1,90 @@
+package com.aimmac23.hub;
+
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
+
+import java.util.Arrays;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.socket.PortFactory;
+import org.openqa.grid.internal.utils.configuration.GridHubConfiguration;
+import org.openqa.grid.web.Hub;
+import org.openqa.selenium.remote.internal.HttpClientFactory;
+import org.testng.Assert;
+
+import com.aimmac23.hub.servlet.HubVideoDownloadServlet;
+import com.aimmac23.hub.servlet.HubVideoInfoServlet;
+
+public class HubServletIntegrationTest {
+	
+	private ClientAndServer mockServer;
+	private Hub hub;
+	private int hubPort;
+	private HttpClient httpClient;
+
+	private int mockClientPort;
+	
+	@Before
+	public void setup() throws Exception {
+		hubPort = PortFactory.findFreePort();
+		mockClientPort = PortFactory.findFreePort();
+
+		// don't strictly need this for the test, but it absorbs any HTTP requests the Hub makes
+	    mockServer = startClientAndServer(mockClientPort);
+	    GridHubConfiguration hubConfig = new GridHubConfiguration();
+	    
+	    hubConfig.servlets = Arrays.asList(HubVideoDownloadServlet.class.getCanonicalName(),
+	    		HubVideoInfoServlet.class.getCanonicalName());
+	    hubConfig.port = hubPort;
+	    
+	    
+	    hub = new Hub(hubConfig);
+	    hub.start();
+	    
+	    httpClient = new HttpClientFactory().getHttpClient();
+
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+	    mockServer.stop();
+	    hub.stop();
+	}
+	
+	@Test
+	public void testVideoInfoServletInitialised() throws Exception {
+		HttpGet get = new HttpGet("http://127.0.0.1:" + hubPort + "/grid/admin/HubVideoInfoServlet/");
+		 HttpResponse response = httpClient.execute(get);
+		 try {
+			 Assert.assertEquals(response.getStatusLine().getStatusCode(), 400);
+			 String entity = EntityUtils.toString(response.getEntity());
+			 // we didn't pass a session ID
+			 Assert.assertTrue(entity.contains("Missing parameter"));
+		 } finally {
+			 get.releaseConnection();
+		 }
+	}
+	
+	@Test
+	public void testVideoDownloadServletInitialised() throws Exception {
+		HttpGet get = new HttpGet("http://127.0.0.1:" + hubPort + "/grid/admin/HubVideoDownloadServlet/");
+		 HttpResponse response = httpClient.execute(get);
+		 try {
+			 Assert.assertEquals(response.getStatusLine().getStatusCode(), 400);
+			 String entity = EntityUtils.toString(response.getEntity());
+			 // we didn't pass a session ID
+			 Assert.assertTrue(entity.contains("Missing parameter"));
+		 } finally {
+			 get.releaseConnection();
+		 }
+	}
+	
+	
+
+}
